a7e232b4d14b0ac3a13f49dc71473cffb13f58c4
jerryxu <jerryxu521@gmail.com>
2020-07-31 01:39:10 +0800

    111

---
+++ .fatherrc.js
@@ -0,0 +1,55 @@
+export default {
+    entry: 'src/index.js',
+    doc: {
+        themeConfig: { mode: 'dark' },
+        base: '/your-repo'
+    },
+    file: 'autohome',
+    esm: {
+        type: 'rollup',
+        file: 'autohome1',
+        mjs: true,
+        minify: true,
+        importLibToEs: true,
+    },
+    cjs: {
+        type: 'rollup',
+        file: 'autohome',
+        lazy: false,
+    },
+    umd: {
+        name: 'autohome'
+    },
+    autoprefixer: {
+        browsers: [
+            'ie>8',
+            'Safari >= 6',
+        ],
+    },
+    cssModules: {
+        generateScopedName: 'autohome_[name]__[local]___[hash:base64:5]',
+    },
+    extractCSS: true,
+    extraBabelPresets: [],
+    extraBabelPlugins: [
+        ['babel-plugin-import', {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true,
+        }],
+    ],
+    extraPostCSSPlugins: [],
+    extraRollupPlugins: [],
+    disableTypeCheck: false,
+    target: "browser",
+    runtimeHelpers: true,
+    replace: {
+        VERSION: JSON.stringify(require('./package').version),
+    },
+    inject: {
+        'window.foo': 'foo',
+    },
+    lessInRollupMode: {},
+    lessInBabelMode: true,
+    sassInRollupMode: {},
+}
